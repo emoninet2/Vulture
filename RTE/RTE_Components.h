5ca77a9773108f10238502d45a1302f62001f299
emon <emon.1992@live.com>
2017-03-25 19:28:52 +0400

    added blank working project

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,38 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: Vulture
+ * RTE configuration: Vulture.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_FreeRTOS        /* CMSIS-RTOS2 FreeRTOS */
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+#define RTE_DEVICE_HAL_ADC
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DAC
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_I2C
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_UART
+#define RTE_DEVICE_HAL_USART
+#define RTE_RTOS_FreeRTOS_CONFIG_RTOS2  /* RTOS FreeRTOS Config for CMSIS RTOS2 API */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+#define RTE_RTOS_FreeRTOS_COROUTINE     /* RTOS FreeRTOS Co-routines */
+#define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+
+#endif /* RTE_COMPONENTS_H */
